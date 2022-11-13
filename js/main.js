c89ccc90a975973470e1cae80d20c8ce9da1de8e
lilaj feiguin <lilaj.feiguin@gmail.com>
2022-11-13 16:16:48 -0300

    nuevo intento de ventana modal

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
+
+if(document.getElementById("buttonModal")) {
+    var modal = document.getElementById ("myModal");
+    var button = document.getElementById ("buttonModal");
+    var span = document.getElementsByClassName ("close")[0];
+    var body = document.getElementsByTagName ("body");
+
+    button.onclick = function () {
+        modal.style.display = "block";
+        body.style.position = "static";
+        body.style.height = "100%";
+        body.style.overflow = "hidden";
+    }
+
+    span.onclick = function () {
+        modal.style.display = "none";
+        body.style.position = "inherit";
+        body.style.height = "auto";
+        body.style.overflow = "visible";
+    }
+
+}
+
+/**
 const openModal = document.querySelector ('.foto-1');
-/*constande modal es igual a la seleccion del elemento que quiero que abra la ventana modal cdo hago click*/
+/*constande modal es igual a la seleccion del elemento que quiero que abra la ventana modal cdo hago click
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.modal-close');
 
@@ -8,9 +32,9 @@ openModal.addEventListener('click', (e)=> {
     modal.classList.add('modal-show')
     
 });
-/**cuando se hace click en el elemento se ejecuta esa fx*/
+/**cuando se hace click en el elemento se ejecuta esa fx
 
 closeModal.addEventListener('click', (e)=> {
     e.preventDefault();
     modal.classList.remove('modal-show')
-});
+});*/
